421a8ca999b7e2713c4105cd5dbb418c878757cf
kadraman <klee2@opentext.com>
2026-02-18 18:52:22 +0000

    chore: updated after PR review

--- packages/cli/jest.config.cjs
+++ packages/cli/jest.config.cjs
@@ -8,4 +8,13 @@ module.exports = {
   resolver: '<rootDir>/../../scripts/jest-resolver.cjs',
   // Use the engine package setup file to keep shared test setup
   setupFilesAfterEnv: ['<rootDir>/../engine/tests/setupTests.ts'],
+  // Transform @beatbax/engine ESM files (don't ignore them like other node_modules)
+  transformIgnorePatterns: [
+    'node_modules/(?!@beatbax/engine)',
+  ],
+  // Use ts-jest to transform .js files from @beatbax/engine
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'ts-jest',
+  },
 };
